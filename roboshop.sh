8275a194899fff718fe39778016a710ed57fc5ca
lakshmi <lakshminarasimha228@gmail.com>
2025-07-08 14:30:46 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -14,11 +14,11 @@ do
         --output text)
         if [ $instance =! "frontend" ]
         then
-            IP=aws ec2 describe-instances --instances -instance-ids $INSTANCE_ID --query "Resrevation[0].
-            Instances[0].PrivateIpAddress" --output text
+            IP=$(aws ec2 describe-instances --instances -instance-ids $INSTANCE_ID --query "Resrevation[0].
+            Instances[0].PrivateIpAddress" --output text)
         else
-            IP=aws ec2 describe-instances --instances -instance-ids $INSTANCE_ID --query "Resrevation[0].
-            Instances[0].PublicIpAddress" --output text
+            IP=$(aws ec2 describe-instances --instances -instance-ids $INSTANCE_ID --query "Resrevation[0].
+            Instances[0].PublicIpAddress" --output text)
         fi
         echo "$instance IP address: $IP"    
 done
