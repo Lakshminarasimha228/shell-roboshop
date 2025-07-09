a5765629d48319a0bcd526a0453a6d26362dbf1b
lakshmi <lakshminarasimha228@gmail.com>
2025-07-09 12:30:20 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -7,9 +7,9 @@ ZONE_ID="Z04271653U7W4X8SKWK80" # replace with your ZONE ID
 DOMAIN_NAME="lakshmi.cyou" # replace with your domain
 
 #for instance in ${INSTANCES[@]}
-for instance in ${instances[@]}
+for instance in ${INSTANCES[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-032739b3c91414e33 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-032739b3c91414e33 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
