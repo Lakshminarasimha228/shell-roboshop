ea14d8d0ba90a7eed74835951ba02035ec297c34
lakshmi <lakshminarasimha228@gmail.com>
2025-08-12 09:42:27 +0530

    shell

--- roboshop.sh
+++ roboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-032a8c7100166b380"  # replace with your SG ID
+SG_ID="sg-032739b3c91414e33"  # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z04271653U7W4X8SKWK80" # replace with your ZONE ID
 DOMAIN_NAME="lakshmi.cyou" # replace with your domain
@@ -9,7 +9,7 @@ DOMAIN_NAME="lakshmi.cyou" # replace with your domain
 #for instance in ${INSTANCES[@]}
 for instance in ${INSTANCES[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.small --security-group-ids sg-032a8c7100166b380 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.small --security-group-ids sg-032739b3c91414e33 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
